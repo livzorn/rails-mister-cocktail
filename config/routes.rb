51833775171d2dbbc03095b37ba428b78946f73a
Olivia Zorn <livzorn@gmail.com>
2020-11-13 15:16:19 +0000

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
   resources :cocktails, only: %i[index create new show] do
     resources :doses, only: %i[create new]
   end
