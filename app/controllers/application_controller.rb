b60b1e18e5248d402cec47b56808400d79b8e9d8
Olivia Zorn <livzorn@gmail.com>
2020-12-21 22:56:32 +0000

    added devise views and new routes to reconfigured pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,17 +7,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
-  private
-    def after_sign_out_path_for(resource_or_scope)
-    landing_path
-  end
-
-  def after_sign_up_path_for(resource)
-    "/quiz" # <- Path you want to redirect the user to.
-  end
-
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
-  end
 end
