ff3d1b8f1eacc0d489ae88cedf09b9872bdc78ee
Olivia Zorn <livzorn@gmail.com>
2020-12-21 23:03:37 +0000

    added routes and pages controller methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,41 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
+  def home
+    @cocktails = Cocktail.all
+  end
+
+  def admin
+    @cocktail = Cocktail.new(strong_params)
+    if @cocktail.save
+      render 'admin'
+    else
+      render :new
+    end
+
+    @ingredient = Ingredient.new(strong_params)
+    if @ingredient.save
+      render 'admin'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def review
+
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :photo)
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
 end
