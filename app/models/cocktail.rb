73c19b4569e2754e00fe167a55aacddb89b2a40d
Olivia Zorn <livzorn@gmail.com>
2021-01-15 23:56:55 +0000

    adjusted spacing on cards

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,7 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy # when you delete a cocktail, associated doses are deleted
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
   has_one_attached :photo
 end
 # rails generate model Doses description cocktail:references ingredient:references
